da83613f771bce68b5aa7e037fc9214afebcee64
matthew17754 <matmoradi17@gmail.com>
2018-05-18 20:12:53 -0700

    Initial Commit
    Implements the pictureBox recorder and the Capture + Record Butons

---
+++ Snippy/Snippy/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Snippy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.btnRecord = new System.Windows.Forms.Button();
+            this.CaptureScrn = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.CaptureScrn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Location = new System.Drawing.Point(860, 12);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(75, 23);
+            this.btnCapture.TabIndex = 0;
+            this.btnCapture.Text = "Capture";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // btnRecord
+            // 
+            this.btnRecord.Location = new System.Drawing.Point(860, 41);
+            this.btnRecord.Name = "btnRecord";
+            this.btnRecord.Size = new System.Drawing.Size(75, 23);
+            this.btnRecord.TabIndex = 1;
+            this.btnRecord.Text = "Record";
+            this.btnRecord.UseVisualStyleBackColor = true;
+            this.btnRecord.Click += new System.EventHandler(this.btnRecord_Click);
+            // 
+            // CaptureScrn
+            // 
+            this.CaptureScrn.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.CaptureScrn.Location = new System.Drawing.Point(13, 12);
+            this.CaptureScrn.Name = "CaptureScrn";
+            this.CaptureScrn.Size = new System.Drawing.Size(841, 513);
+            this.CaptureScrn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.CaptureScrn.TabIndex = 2;
+            this.CaptureScrn.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 537);
+            this.Controls.Add(this.CaptureScrn);
+            this.Controls.Add(this.btnRecord);
+            this.Controls.Add(this.btnCapture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.CaptureScrn)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.Button btnRecord;
+        private System.Windows.Forms.PictureBox CaptureScrn;
+    }
+}
+
---
+++ Snippy/Snippy/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snippy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+            Bitmap btm = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics grph = Graphics.FromImage(btm);
+            grph.CopyFromScreen(0, 0, 0, 0, btm.Size);
+            CaptureScrn.Image = btm;
+        }
+
+        void Capture()
+        {
+            while (true)
+            {
+                Bitmap btm = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+                Graphics grph = Graphics.FromImage(btm);
+                grph.CopyFromScreen(0, 0, 0, 0, btm.Size);
+                CaptureScrn.Image = btm;
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void btnRecord_Click(object sender, EventArgs e)
+        {
+            Thread t = new Thread(Capture);
+            t.Start();
+        }
+    }
+}
